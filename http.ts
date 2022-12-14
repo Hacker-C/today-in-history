151621ca5537c1942c68ab56a1c3a24df11781a0
Hacker-C <1507559148@qq.com>
2022-12-14 14:48:28 +0800

    🔧 chore: format code

--- http.ts
+++ http.ts
@@ -40,7 +40,7 @@ const theResponse = (
 ) => {
 	return new Response(JSON.stringify({ status, message, data: dayData }), {
 		headers,
-    status
+		status,
 	});
 };
 
@@ -49,15 +49,15 @@ async function handler(req: Request): Promise<Response> {
 	const params = new URL(req.url).searchParams;
 	const [month, day] = [params.get('month'), params.get('day')];
 
-  if (month === null && day !== null || month !== null && day === null) {
-    // ! 一个传了一个没传
-    return theResponse(null, 400, '请传入正确的参数！');
-  }
+	if (month === null && day !== null || month !== null && day === null) {
+		// ! 一个传了一个没传
+		return theResponse(null, 400, '请传入正确的参数！');
+	}
 
 	let monthArg: string, dayArg: string, queryArg: string;
 	// 都没有传则默认返回当天数据
 	if (month === null && day === null) {
-    // ! 两个都没传
+		// ! 两个都没传
 		// 获取东八区时间（GMT+8）
 		const date = new Intl.DateTimeFormat('zh-CN', { timeZone: 'Asia/Shanghai' }).format();
 		// date -> mm/dd/yy
@@ -66,10 +66,10 @@ async function handler(req: Request): Promise<Response> {
 		dayArg = (d.length < 2 ? '0' + d : d) as string;
 		queryArg = monthArg + '' + dayArg;
 	} else if (!/^(?:1[0-2]|[1-9])$/.test(month!) || !/^([1-9]|[1-2]\d|3[0-1])$/.test(day!)) {
-    // ! 传了但是格式不对
+		// ! 传了但是格式不对
 		return theResponse(null, 400, '请传入正确的参数！');
-  } else {
-    // ! 传了且格式正确
+	} else {
+		// ! 传了且格式正确
 		monthArg = month!.length < 2 ? '0' + month : month as string;
 		dayArg = day!.length < 2 ? '0' + day : day as string;
 		queryArg = monthArg! + dayArg!;
@@ -92,7 +92,7 @@ async function handler(req: Request): Promise<Response> {
 	const monthData: Record<string, EventType[] | null> = originData![monthArg!];
 	const dayData: EventType[] | null = monthData[queryArg];
 	if (!dayData || dayData.length === 0) {
-    // ! 月和日不匹配
+		// ! 月和日不匹配
 		return theResponse(null, 404, 'month 和 day 不匹配！');
 	}
 
